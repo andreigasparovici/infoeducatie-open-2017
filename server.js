4ab14895f2e1929a0d00d0baab5df8eeb03cce61
Andrei Gasparovici <andreigasparovici1@gmail.com>
2017-08-03 23:00:44 +0300

    Updated login & signup

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 
+const dbConnection = require('./dbapi/connection.js');
+const DbApi = require('./dbapi/api.js');
+
+let dbApi = new DbApi(dbConnection);
+
 const app = express();
 
 require('dotenv').config();
@@ -41,8 +46,64 @@ app.get('/login', (req, res) => {
 	res.render("login");
 });
 
+app.post('/login', (req, res) => {
+	if(!req.body.email) {
+		req.flash('error', 'Adresă de e-mail invalidă!');
+		return res.redirect('/login');
+	}
+
+	if(!req.body.password) {
+		req.flash('error', 'Parolă invalidă!');
+		return res.redirect('/login');
+	}
+
+	dbApi.loginUser(req.body.email, req.body.password)
+		.then((result) => {
+			if(result.is_teacher && !result.activated) {
+				req.flash('error', 'Contul aşteaptă aprobare de la un moderator');
+				return res.redirect('/login');
+			}
+			req.session.user = {
+				email: req.body.email,
+				name: result.name,
+				id: result.id,
+				is_teacher: result.is_teacher
+			};
+			return res.redirect('/');
+		})
+		.catch(() => {
+			req.flash('error', 'Date de autentificare incorecte!');
+			return res.redirect('/login');
+		});
+});
+
 app.get('/signup', (req, res) => {
 	res.render("signup");
+});
+
+app.post('/signup', (req, res) => {
+	if(!req.body.name) {
+		req.flash('error', 'Nume invalid!');
+		return res.redirect('/login');
+	}
+
+	if(!req.body.email) {
+		req.flash('error', 'Email invalid!');
+		return res.redirect('/login');
+	}
+
+	if(!req.body.password) {
+		req.flash('error', 'Parolă invalidă');
+		return res.redirect('/login');
+	}
+
+	if(req.body.password_confirm != req.body.password) {
+		req.flash('error', 'Parola trebuie confirmată');
+		return res.redirect('/login');
+	}
+
+	res.json(req.body);
+	//dbApi.signupUser(req.body.email, bcrypt.hashSync(req.body.password), req.body.name, req.body.is_teacher)
 });
 
 app.get('/lectii', checkAuth, (req, res) => {
