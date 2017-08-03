27494af81bfe2ded13454ca82b987792d0f42cc8
Timi <timitoc@yahoo.com>
2017-08-03 13:59:41 +0300

    Implemented declaration conversion

--- Converter.js
+++ Converter.js
@@ -11,7 +11,7 @@ var Edge = function(i, j, t) {
 var Block = function(e, t) {
     this.expr = e;
     this.type = t;
-    this.id = ++globalIdCount;
+    this.id = "e" + (++globalIdCount);
     this.edges = [];
 }
 
@@ -21,17 +21,56 @@ var addEdge = function(a, b, t) {
 }
 
 var parseCursor;
+var phpCode;
 
+var isWhite = function(c) {
+    if (c == ' ' || c == '\t' || c == '\n')
+        return true;
+    return false;
+}
+
+var slideWhites = function() {
+    while (parseCursor < phpCode.length && isWhite(phpCode[parseCursor]))
+        parseCursor++;
+}
+
+var getToSemicolon = function() {
+    var news = "";
+    while (parseCursor < phpCode.length && phpCode[parseCursor] != ';')
+        news += phpCode[parseCursor++];
+    parseCursor++;
+    return news;
+}
+
+var eliminateHolders = function(text) {
+    var news = "";
+    for (var i = 0; i < text.length; i++)
+        if (text[i] != '$')
+            news += text[i];
+    return news;
+}
+
+/// Creates execution graph from php code
 var convert = function(phpText) {
     this.blocks = [];
     parseCursor = 0;
+    phpCode = phpText;
     this.blocks.push(new Block("START", "START"));
-    addEdge(this.blocks[0], parsePhpCode(phpText), "EMPTY");
-    return phpText;
+    addEdge(this.blocks[0], parsePhpCode(phpCode), "EMPTY");
+    return phpCode;
 }
 
-var parsePhpCode = function(phpText) {
-    
+var parsePhpCode = function() {
+    slideWhites();
+    if (parseCursor >= phpCode.length)
+        return new Block("", "EMPTY");
+    if (phpCode[parseCursor] == '$') {
+        var expression = getToSemicolon()
+        expression = eliminateHolders(expression);
+        if (!expression.includes('='))
+            return new Block("", "EMPTY");
+        return new Block(expression, "operation"); 
+    }
 }
 
 var phpText = fs.readFileSync('phptestfile.php');
