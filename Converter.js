3b40e9513f543ea298d5113f3f587f882a242adc
Timi <timitoc@yahoo.com>
2017-08-03 13:10:34 +0300

    Edge construction

--- Converter.js
+++ Converter.js
@@ -1,7 +1,37 @@
 var fs = require('fs');
+var globalIdCount = 0;
+
+/// Edge between i and j with type t
+var Edge = function(i, j, t) {
+    this.x = i;
+    this.y = j;
+    this.type = t; /// DA, NU, EMPTY
+}
+
+var Block = function(e, t) {
+    this.expr = e;
+    this.type = t;
+    this.id = ++globalIdCount;
+    this.edges = [];
+}
+
+var addEdge = function(a, b, t) {
+    a.edges.push(new Edge(a.id, b.id, t));
+    b.edges.push(new Edge(b.id, a.id, t));
+}
+
+var parseCursor;
 
 var convert = function(phpText) {
+    this.blocks = [];
+    parseCursor = 0;
+    this.blocks.push(new Block("START", "START"));
+    addEdge(this.blocks[0], parsePhpCode(phpText), "EMPTY");
     return phpText;
+}
+
+var parsePhpCode = function(phpText) {
+    
 }
 
 var phpText = fs.readFileSync('phptestfile.php');
